38212e47ed34ab55272613003c3616353aeb8f17
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2024-07-05 15:12:49 +0200

    Finish Embedding

--- crates/ratchet-models/src/bert/bert_embedding.rs
+++ crates/ratchet-models/src/bert/bert_embedding.rs
@@ -1,18 +1,22 @@
 
-use ratchet::{shape, DType, Device, Tensor};
+use ratchet::{shape, Device, Tensor};
 use ratchet_loader::gguf::gguf::Header;
-use ratchet_nn::{Embedding, KVCache, KVEntry, LayerNorm, Linear, Module};
-
+use ratchet_nn::{Embedding,LayerNorm, Module};
+use std::io::{BufRead, Seek};
 
 pub struct EmbeddingInput{
     input_ids: Tensor,
     token_type_ids: Tensor
 }
 
+#[cfg(target_arch = "wasm32")]
+use {crate::ratchet_from_gguf_web, crate::TensorMap};
+
 pub struct BertEmbedding{
     token_type_embedding: Embedding,
     position_embedding: Embedding,
-    word_embedding: Embedding
+    word_embedding: Embedding,
+    norm:LayerNorm
 }
 
 impl Module for BertEmbedding{
@@ -20,12 +24,12 @@ impl Module for BertEmbedding{
 
     fn schedule(&self, input: Self::Input) -> anyhow::Result<Tensor> {
         let [_, seq_len]: [usize; 2] = input.input_ids.shape().try_into()?;
+
         let input_embeddings = self.word_embedding.schedule(input.input_ids)?;
         let token_type_embeddings = self.token_type_embedding.schedule(input.token_type_ids)?;
 
         let embeddings = input_embeddings.add(token_type_embeddings)?;
 
-        
         let position_ids = (0..seq_len as u32).collect::<Vec<_>>();
         let position_ids = Tensor::from_data(
             position_ids,
@@ -34,9 +38,36 @@ impl Module for BertEmbedding{
         );
         let position_embeddings = self.position_embedding.schedule(position_ids)?;
 
+        // Will `add` broadcast here?
         let embeddings = embeddings.add(position_embeddings)?;
 
+        let embeddings = self.norm.schedule(embeddings)?;
         Ok(embeddings)
     }
 
+}
+
+impl BertEmbedding {
+    pub fn load<R: BufRead + Seek>(
+        disk_model: &Header,
+        reader: &mut R,
+        device: &Device,
+    ) -> anyhow::Result<Self> {
+
+        let eps: f32 = disk_model.metadata.get("bert.attention.layer_norm_epsilon").unwrap().to_f32()?;
+        let token_type_embedding = Embedding::new(disk_model.tensor(reader, "token_types.weight", device)?);
+        let position_embedding = Embedding::new(disk_model.tensor(reader, "position_embd.weight", device)?);
+        let word_embedding = Embedding::new(disk_model.tensor(reader, "token_embd.weight", device)?);
+        let norm = LayerNorm::new(
+            disk_model.tensor(reader, "token_embd_norm.weight", device)?,
+            Some(disk_model.tensor(reader, "token_embd_norm.bias", device)?),
+            eps
+        );
+        Ok(Self{
+            token_type_embedding,
+            position_embedding,
+            word_embedding,
+            norm
+        })
+    }
 }
