0c8ac5e6b55f851d775ec461954d36b758792622
Lukas Kreussel <65088241+LLukas22@users.noreply.github.com>
2024-07-21 11:11:07 +0200

    add some `hnswlib` stuff

--- examples/ratchet-bert/src/routes/+page.svelte
+++ examples/ratchet-bert/src/routes/+page.svelte
@@ -1,39 +1,81 @@
 <script>
-import { AutoModel, AutoTokenizer } from '@xenova/transformers';
-import { Model, DecodingOptionsBuilder, default as init, Quantization, Segment } from "@ratchet-ml/ratchet-web";
+    // import { AutoModel, AutoTokenizer } from '@xenova/transformers';
+    import { Model, DecodingOptionsBuilder, default as init, Quantization, Segment } from "@ratchet-ml/ratchet-web";
+    import { loadHnswlib, syncFileSystem } from 'hnswlib-wasm';
 
-  /**
-   * @type {Model | null}
+    import { createRepo, uploadFiles, uploadFilesWithProgress, deleteFile, deleteRepo, listFiles, whoAmI } from "@huggingface/hub";
+
+    /**
+   * @type {import("hnswlib-wasm").HnswlibModule | null}
    */
-let model = null;
+    let hnswlib = null;
 
-async function load_model(){
+    /**
+     * @type {Model | null}
+     */
+    let model = null;
 
-    await init();
-    model = await Model.load( { Bert:"mini_l_m"} ,Quantization.F32, (/** @type {any} */ p) => console.log(p));
-    console.log(model);
+    /**
+   * @param {string} name
+   * @param {number} dimension
+   */
+    async function create_index(name,dimension){
+        if (!hnswlib){
+            console.debug('Loading hnswlib...');
+            hnswlib = await loadHnswlib();
+            hnswlib.EmscriptenFileSystemManager.setDebugLogs(true);
+        }
 
-    // if(!tokenizer){
-    //     console.log('loading tokenizer');
-    //     tokenizer = await AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2');
-    // }
-
-    // if(!model){
-    //     console.log('loading model');
-    //     model = await AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2');
-    // }
-}
-
-async function embed(){
-    const text1 = 'Why did the crab cross the road?';
-    const text2  = 'Another unrelated prompt';
-    if (!model){
-        console.log('no model loaded');
-        return;
+        let index = new hnswlib.HierarchicalNSW('cosine', dimension,name);
+        await syncFileSystem('read');
+        const exists = hnswlib.EmscriptenFileSystemManager.checkFileExists(name);
+        if (!exists) {
+            index.initIndex(100000, 48, 128, 100);
+            index.setEfSearch(32);
+            await index.writeIndex(name);
+        } else {
+            await index.readIndex(name, 100000);
+            index.setEfSearch(32);
+        }
     }
-    const tokens  = await model.run({texts: Array(32).fill(text1)});
-    console.log([text1, text2]);
-    console.log(tokens);
+
+
+    async function load_model(){
+
+        await init();
+        model = await Model.load( { Bert:"mini_l_m"} ,Quantization.F32, (/** @type {any} */ p) => console.log(p));
+        console.log(model);
+
+        await create_index("foo.dat", 384);
+      
+
+        console.log(hnswlib);
+        // if(!tokenizer){
+        //     console.log('loading tokenizer');
+        //     tokenizer = await AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2');
+        // }
+
+        // if(!model){
+        //     console.log('loading model');
+        //     model = await AutoModel.from_pretrained('sentence-transformers/all-MiniLM-L6-v2');
+        // }
+    }
+
+    async function embed(){
+        const text1 = 'Why did the crab cross the road?';
+        const text2  = 'Another unrelated prompt';
+        if (!model){
+            console.log('no model loaded');
+            return;
+        }
+        const tokens  = await model.run({texts: Array(32).fill(text1)});
+        console.log([text1, text2]);
+        console.log(tokens);
+
+        if (!hnswlib){
+            console.log('no db loaded');
+            return;
+        }
 }
 
 </script>
